bbd9084538425ab63fc878f602b825e74390cb30
github username <github email address>
2019-01-31 15:13:23 -0500

    added status bar with play button, only alerts [play game]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Puzzle from './Puzzle'
+import StatusBar from './StatusBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-            <h1>Memory Match</h1>
+            <h1>MEMORY MATCH</h1>
         </div>
-        <div className="Puzzle-body">
+        <StatusBar />
+        <div className="Puzzle-body" id="Puzzle-body">
+
           <Puzzle />
         </div>
       </div>
